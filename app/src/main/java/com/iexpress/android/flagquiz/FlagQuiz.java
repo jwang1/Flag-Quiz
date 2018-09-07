ccc52503141ab9a0d985fd8f2a5ee9043ed80a35
jwang1 <sigkdd@yahoo.com>
2018-09-06 23:00:10 -0400

    fixing null pointer caused by id typo in QuizFlagFragment
    added some code in FlagQuizActivity

--- app/src/main/java/com/iexpress/android/flagquiz/FlagQuiz.java
+++ app/src/main/java/com/iexpress/android/flagquiz/FlagQuiz.java
@@ -1,5 +1,6 @@
 package com.iexpress.android.flagquiz;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
@@ -54,23 +55,42 @@ public class FlagQuiz extends AppCompatActivity {
     }
 
     @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (preferencesChanged) {
+            // default preferences already set; init Flag fragment
+            FlagQuizFragment quizFragment =
+                (FlagQuizFragment) getSupportFragmentManager().findFragmentById(R.id.quizFragment);
+
+            quizFragment.updateGuessRows(PreferenceManager.getDefaultSharedPreferences(this));
+
+            quizFragment.updateRegions(PreferenceManager.getDefaultSharedPreferences(this));
+
+            quizFragment.resetQuiz();
+
+            preferencesChanged = false;
+        }
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_flag_quiz, menu);
-        return true;
+        // current orientation
+        int orientation = getResources().getConfiguration().orientation;
+
+        if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+            // Inflate the menu; this adds items to the action bar if it is present.
+            getMenuInflater().inflate(R.menu.menu_flag_quiz, menu);
+            return true;
+        }
+
+        return false;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        Intent preferencesIntent = new Intent(this, SettingsActivity.class);
+        startActivity(preferencesIntent);
 
         return super.onOptionsItemSelected(item);
     }
--- app/src/main/java/com/iexpress/android/flagquiz/FlagQuizFragment.java
+++ app/src/main/java/com/iexpress/android/flagquiz/FlagQuizFragment.java
@@ -1,16 +1,45 @@
 package com.iexpress.android.flagquiz;
 
 import android.content.SharedPreferences;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.security.SecureRandom;
+import java.util.List;
+import java.util.Set;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class FlagQuizFragment extends Fragment {
+    private static final String TAG = "FlagQuiz Activity";
+
+    private static final int FLAGS_IN_QUIZ = 10;
+
+    private List<String> fileNameList;
+    private List<String> quizCountriesList;
+    private Set<String> regionsSet;
+    private String correctAnswer;
+    private int totalGuesses;
+    private int correctAnswers;
+    private int guessRows;
+    private SecureRandom random;
+    private Handler handler;  // to delay loading next flag
+    private Animation shakeAnimation;
+
+    private LinearLayout quizLinearLayout;
+    private TextView questionNumberTextView;
+    private ImageView flagImageView;
+    private LinearLayout[] guessLinearLayouts;
+    private TextView answerTextView;
 
     public FlagQuizFragment() {
     }
