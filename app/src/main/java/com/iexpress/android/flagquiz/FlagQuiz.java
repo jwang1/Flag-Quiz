59e03abcdb21ef8fa6af58a2763ff4d81f4b2271
jwang1 <sigkdd@yahoo.com>
2018-09-08 21:19:49 -0400

    mark the FloatingActionButton Gone, not to display it

--- app/src/main/java/com/iexpress/android/flagquiz/FlagQuiz.java
+++ app/src/main/java/com/iexpress/android/flagquiz/FlagQuiz.java
@@ -6,7 +6,6 @@ import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
